005937ef02bf2f475204bdbf06599530293f9216
elamaran-elango <elamaran.elango@freshworks.com>
2021-02-28 16:06:14 +0530

    add sysinit

---
+++ sysinit.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
