3f4173a8c95b977a98fcd5adee6c6e7442dbe82a
elamaran-elango <elamaran.elango@freshworks.com>
2021-02-28 17:44:39 +0530

    add trackpad control

--- sysinit.sh
+++ sysinit.sh
@@ -1,3 +1,14 @@
 mkdir -p ~/.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+# add following lines in /etc/X11/xorg.conf.d/30-touchpad.conf
+
+# Section "InputClass"
+# 	Identifier "libinput touchpad catchall"
+# 	MatchIsTouchpad "on"
+# 	MatchDevicePath "/dev/input/event*"
+# 	Driver "libinput"
+# 	Option "NaturalScrolling" "true"
+# 	Option "ClickMethod" "clickfinger"
+# 	Option "Tapping" "on"
+# EndSection
